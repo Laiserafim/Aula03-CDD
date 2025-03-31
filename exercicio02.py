6cc99bd3b00b7819e39389d964b193bca912e73e
Laís Serafim <laisserafim8@gmail.com>
2025-03-31 09:50:29 -0300

    Enviando exercício02

---
+++ exercicio02.py
@@ -0,0 +1,11 @@
+numero01 = float(input("Digite o primeiro número: "))
+numero02 = float(input("Digite o segundo número: "))
+soma = numero01 + numero02
+subtracao= numero01 - numero02
+divisao = numero01 / numero02
+multiplicacao = numero01 * numero02
+
+print(f" Soma: {numero01} + {numero02} = {soma}")
+print(f" Subtração: {numero01} - {numero02} = {subtracao}")
+print(f" Divisão: {numero01} / {numero02} = {divisao}")
+print(f" Multiplicação: {numero01} * {numero02} = {multiplicacao}")
